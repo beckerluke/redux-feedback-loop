ce9bba375ed7a80615b0e8dc2bf3cbeef6559650
Luke Becker <beckerluke1@gmail.com>
2019-08-30 15:53:48 -0500

    created reducer folder,services folder, store inside index.js and imported redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// REDUCERS
+import baseReducer from './components/modules/reducers/base.reducer'
+
+
+const store = createStore(
+    combineReducers({
+        baseReducer,
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();

3f6d9f70e7c69c1f30f60a53116d1fb894651d26
Luke Becker <beckerluke1@gmail.com>
2019-08-30 20:43:06 -0500

    mapped through feedbackReducer state and displayed results on Review page component

--- src/components/pages/reviewPage/ReviewPage.js
+++ src/components/pages/reviewPage/ReviewPage.js
@@ -21,9 +21,20 @@ class ReviewPage extends Component {
 
 
     render() {
+        const feedbackArray = this.props.store.feedbackReducer.map((feedback, i) => {
+            return (
+                <div key={i}>
+                    <h3>Feelings: {feedback.feeling}</h3>
+                    <h3>Understanding: {feedback.understanding}</h3>
+                    <h3>Support: {feedback.support}</h3>
+                    <h3>Comments: {feedback.comments}</h3>
+                </div>
+            )
+        })
         return(
             <div>
                 <h1>Review Your Feedback</h1>
+                {feedbackArray}
                  {/* Displays User's Feelings Input Data
                      Displays User's Understanding Input Data
                      Displays User's Support Input Data
